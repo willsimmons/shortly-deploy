d8ca462c41e07455edaaf94ebfc373c34e6d6041
willsimmons <willsimmons@gmail.com>
2016-10-13 15:28:26 -0700

    Disconnect from sqlite and create connection/schemas for MongoDB

--- app/config.js
+++ app/config.js
@@ -1,40 +1,38 @@
 var path = require('path');
-var knex = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  },
-  useNullAsDefault: true
-});
-var db = require('bookshelf')(knex);
+// Connection URL - change for live, keep for local
+var url = 'mongodb://localhost:27017/shortly';
+//connect the database to the orm, mongoose
+var mongoose = require('mongoose');
+mongoose.connect(url);
+//need to connect to the mongo db database
+//this connection serves as our way to get and set data from the database
+var db = mongoose.connection;
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connecting to db!');
 });
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+
+var urlSchema = mongoose.Schema({
+  id: ObjectId, // new mongoose.Types.ObjectId;
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number,
+  timestamps: Date // Date.now
 });
+
+var userSchema = mongoose.Schema({
+  id: ObjectId,
+  username: String,
+  password: String,
+  timestamps: Date
+});
+
 
 module.exports = db;
+
+
+
