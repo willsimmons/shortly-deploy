78c35f72199d97d580795dc85ecc7fdb8c84daac
willsimmons <willsimmons@gmail.com>
2016-10-13 21:35:03 -0700

    Pass all tests except ones that require checking against a hashed pw

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,6 @@ var mongoose = require('mongoose');
 
 
 var userSchema = new mongoose.Schema({
-  id: Number,
   username: String,
   password: String,
   timestamps: {type: Date, default: Date.now}
